1935caa9c93e40e6de20a66c3fc55c8a841fe364
Vincent Le Quang <vincent@storm8.com>
2015-11-21 14:35:28 -0800

    First commit

---
+++ GDC/Assets/PlayerItem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace GDC.Player {
+	[RequireComponent(typeof(Text))]
+	public class PlayerItem : MonoBehaviour {
+
+		public int player;
+		// Use this for initialization
+		void Start () {
+			PlayerManager.OnUpdate += HandleOnUpdate;
+		}
+
+		void HandleOnUpdate ()
+		{
+			Player p = PlayerManager.instance.players[player];
+			GetComponent<Text>().text = p.key.ToString();
+		}
+	}
+}
---
+++ GDC/Assets/PlayerManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GDC.Player {
+	public class Player {
+		public KeyCode key = KeyCode.None;
+		public int score;
+	}
+
+	public class PlayerManager : MonoBehaviour {
+		[HideInInspector]
+		public Player[] players;
+
+		public int playerCount;
+
+		public KeyCode[] keys;
+
+		public delegate void UpdateAction();
+		public static event UpdateAction OnUpdate;
+
+		static public PlayerManager instance;
+
+		void Start () {
+			instance = this;
+			players = new Player[playerCount+1];
+		}
+
+		public void Update() {
+			bool needUpdate = false;
+			for(int i=1;i<players.Length;i++) {
+				if(players[i] == null) {
+					players[i] = new Player();
+				}
+				if(players[i].key == KeyCode.None) {
+					players[i].key = keys[Random.Range(0,keys.Length-1)];
+					needUpdate = true;
+				}
+
+				if(Input.GetKeyDown(players[i].key)) {
+					players[i].key = KeyCode.None;
+					players[i].score++;
+					needUpdate = true;
+				}
+			}
+			OnUpdate();
+		}
+	}
+}
